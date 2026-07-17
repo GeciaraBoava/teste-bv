61fe895f41e7d54620d811a4306896a30e49ffa3
GeciaraBoava <geciaracardoso@gmail.com>
2026-07-16 21:41:45 -0300

    🎉  init :: [project] ::  cria projeto Spring Boot

---
+++ src/main/java/com/bv/geciara/GeciaraApplication.java
@@ -0,0 +1,13 @@
+package com.bv.geciara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeciaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeciaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bv/geciara/GeciaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bv.geciara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeciaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
